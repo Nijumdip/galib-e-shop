346b6d787aa7f29b0199c3ad6b94549e9203b2b4
Nijumdip <nijumdip019911@gmail.com>
2022-12-18 13:42:22 +0600

    done navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,8 +3,8 @@ import React from "react";
 const Navigation = () => {
     return (
         <>
-            <div style={{ background: "#00A9E4" }} className="navbar ">
-                <div className="navbar-start">
+            <div style={{ background: "#00A9E4" }} className="navbar">
+                <div className="navbar-start sm:ml-10 ml-0">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg
@@ -27,11 +27,11 @@ const Navigation = () => {
                             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
                         >
                             <li>
-                                <a>Item 1</a>
+                                <a>Home</a>
                             </li>
                             <li tabIndex={0}>
                                 <a className="justify-between">
-                                    Parent
+                                    Product
                                     <svg
                                         className="fill-current"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -44,65 +44,131 @@ const Navigation = () => {
                                 </a>
                                 <ul className="p-2">
                                     <li>
-                                        <a>Submenu 1</a>
+                                        <a>Laptop</a>
                                     </li>
                                     <li>
-                                        <a>Submenu 2</a>
+                                        <a>Foods</a>
                                     </li>
                                 </ul>
                             </li>
                             <li>
-                                <a>Item 3</a>
+                                <a>AboutUs</a>
+                            </li>
+                            <li>
+                                <a>ContactUs</a>
                             </li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">Galib E Shop</a>
 
-                    
-                        <div className="form-control">
-                            <input
-                                type="text"
-                                placeholder="Search"
-                                className="input input-bordered bg-sky-300"
-                            />
-                        </div>
-                    
-
+                    <div className=" w-full hidden lg:flex ml-20">
+                        <input
+                            type="text"
+                            placeholder="Search here"
+                            className="input w-full focus:bg-white bg-sky-300"
+                        />
+                    </div>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li>
-                            <a>Item 1</a>
-                        </li>
-                        <li tabIndex={0}>
-                            <a>
-                                Parent
-                                <svg
-                                    className="fill-current"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                                </svg>
-                            </a>
-                            <ul className="p-2">
+                <div className="navbar-end sm:mr-10 mr:0">
+                    <div className="hidden lg:flex  justify-evenly">
+                        <ul className="menu menu-horizontal px-1 ">
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li tabIndex={0}>
+                                <a>
+                                    Products
+                                    <svg
+                                        className="fill-current"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                    </svg>
+                                </a>
+                                <ul className="p-2">
+                                    <li>
+                                        <a>Laptop</a>
+                                    </li>
+                                    <li>
+                                        <a>Foods</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a>AboutUs</a>
+                            </li>
+                            <li>
+                                <a>ContactUs</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    
+                    
+                    <div className="flex-none">
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle">
+                                <div className="indicator">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                                        />
+                                    </svg>
+                                    <span className="badge badge-sm indicator-item">8</span>
+                                </div>
+                            </label>
+                            <div
+                                tabIndex={0}
+                                className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+                            >
+                                <div className="card-body">
+                                    <span className="font-bold text-lg">8 Items</span>
+                                    <span className="text-info">Subtotal: $999</span>
+                                    <div className="card-actions">
+                                        <button className="btn btn-primary btn-block">
+                                            View cart
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <a className="">SignIn</a> */}
+
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src="https://placeimg.com/80/80/people" alt="" />
+                                </div>
+                            </label>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                            >
                                 <li>
-                                    <a>Submenu 1</a>
+                                    <a className="justify-between">Profile</a>
                                 </li>
                                 <li>
-                                    <a>Submenu 2</a>
+                                    <a>Settings</a>
+                                </li>
+                                <li>
+                                    <a>Logout</a>
                                 </li>
                             </ul>
-                        </li>
-                        <li>
-                            <a>Item 3</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Get started</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
