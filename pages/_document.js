8d346f23609ef52da45c0257d6fa19d754abe1c5
Nijumdip <nijumdip019911@gmail.com>
2022-12-19 17:12:47 +0600

    added _document.js file

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* fontAwesome */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap"
+              rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
